22d0967b6f503c18a50f03e02e51a5dfa1615097
Alla-Varlamova <beautygerbera@gmail.com>
2025-10-01 21:13:54 +0300

    Event handling

--- StudentCard/MainWindow.xaml.cs
+++ StudentCard/MainWindow.xaml.cs
@@ -23,6 +23,54 @@ namespace StudentCard
         public MainWindow()
         {
             InitializeComponent();
+            btnLoadPhoto.Click += btnLoadPhoto_Click;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFirstName.Text) ||
+                string.IsNullOrEmpty(txtLastName.Text))
+            {
+                MessageBox.Show("Заполните обязательные поля!");
+                return;
+            }
+
+            string gender = rbMale.IsChecked == true ? "Мужской" : "Женский";
+            string message =
+               $"Студент: {txtFirstName.Text} {txtLastName.Text}\n" +
+               $"Возраст: {txtAge.Text}\nПол: {gender}\n" +
+               $"Email: {txtEmail.Text}\nТелефон: {txtPhone.Text}\n" +
+               $"Курс: {(cmbCourse.SelectedItem as ComboBoxItem)?.Content}\n" +
+               $"Специализация: {(cmbSpecialization.SelectedItem as ComboBoxItem)?.Content}";
+
+            MessageBox.Show(message, "Данные сохранены");
+
+        }
+
+        private void btrClear_click(object sender, RoutedEventArgs e)
+        {
+            txtFirstName.Text = string.Empty;
+            txtLastName.Text = string.Empty;
+            txtAge.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtPhone.Text = string.Empty;
+            rbMale.IsChecked = false;
+            rbFemale.IsChecked = false;
+            cmbCourse.SelectedIndex = -1;
+            cmbSpecialization.SelectedIndex = -1;
+            imgPhoto.Source = null;
+        }
+
+        private void btnLoadPhoto_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg, *.png)|*.jpg;*.png";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                imgPhoto.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+
         }
     }
 }
